94e087f8c919585d19537a20019bcc3dfb7e1a1d
Azzahra Cindy <azzahracindy711@gmail.com>
2026-04-15 19:41:31 +0700

    project awal

---
+++ porto.js
@@ -0,0 +1,9 @@
+function menu() {
+  var x = document.getElementById("isiMenu");
+
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
